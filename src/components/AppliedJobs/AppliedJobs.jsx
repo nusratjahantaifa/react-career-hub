b01282ad45a31ba3222383791102e2a847194c98
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2026-01-12 13:47:00 +0600

    added all

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -7,7 +7,7 @@ const AppliedJobs = () => {
    const jobs = useLoaderData();
 const [appliedJobs, setAppliedJobs] = useState([]);
 const [displayJobs, setDisplayJobs] = useState([]);
-   const handleJobsFilter = filter =>{
+   const handleJobsFilter = (filter) =>{
         if(filter === 'all'){
             setDisplayJobs(appliedJobs);
         }
@@ -15,11 +15,11 @@ const [displayJobs, setDisplayJobs] = useState([]);
             const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
             setDisplayJobs(remoteJobs);
         }
-        else if(filter === 'onsite'){
+      else if (filter === 'onsite') {
             const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
             setDisplayJobs(onsiteJobs);
         }
-    }
+    };
 useEffect (() => {
   const storedJobIds = getStoredJobApplication();
         if (jobs.length > 0) {
@@ -41,12 +41,10 @@ useEffect (() => {
             <details className="dropdown mb-32">
                 <summary className="m-1 btn">open or close</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    {/* <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                    <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
                     <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
-                    <li onClick={() => handleJobsFilter('onsite')}><a>onsite</a></li> */}
-<li><a>All</a></li>
-<li><a>Remote</a></li>
-<li><a>onsite</a></li>
+                    <li onClick={() => handleJobsFilter('onsite')}><a>onsite</a></li>
+
                 </ul>
             </details> 
             
